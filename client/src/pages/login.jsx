0338139b3299597d1bbfd0cde4f5be8ceba82ea5
Yurei <glba.alicoben.up@phinmaed.com>
2024-01-16 16:43:01 +0800

    login.jsx

--- client/src/pages/login.jsx
+++ client/src/pages/login.jsx
@@ -1,6 +1,36 @@
-import".../App.css"
+import React from 'react';
+import './login.css';
+import { FaUser, FaLock } from "react-icons/fa";
+
+
 const Login = () => {
-    return <> test lang </>;
+    return (
+        <div className='wrapper'>
+            <form action="">
+                <h1>Login</h1>
+                <div className='input-box'>
+                    <input type="text" placeholder='Username' required />
+                    <FaUser className='icon' />
+                </div>
+                <div className='input-box'>
+                    <input type="password" placeholder='Password' required />
+                    <FaLock className='icon' />
+                </div>
+
+                <div className="remember-forgot">
+                    <label> <input type="checkbox" /> Remember me</label>
+                    <a href="#"> Forgot password?</a>
+                </div>
+                <button type='submit'>Login</button>
+                <div className='register-link'>
+                    <p>Don't have an coount? <a href="#">Register</a></p>
+                </div>
+            </form>
+        </div>
+    );
+
+
+
 };
 
 export default Login;
