3d70369ad769363c581db64308e0aa4e225ff74b
leyschz <lema.delacruz.up@phinmaed.com>
2024-01-27 01:49:28 +0800

    added some changes in "Inbox" and "Task"
    Co-authored-by: Zai A. <Zaisuki@users.noreply.github.com>

--- client/src/pages/inbox.jsx
+++ client/src/pages/inbox.jsx
@@ -1,43 +1,91 @@
-import { useState } from 'react';
-import 'bootstrap/dist/css/bootstrap.min.css';
-import '../assets/scss/inbox.scss';
+import { useState } from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Dropdown from "react-bootstrap/Dropdown";
+import Container from "react-bootstrap/Container";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import profileIcon from "../assets/img/mygirl.jpg";
+import "../assets/scss/inbox.scss";
 
 const Inbox = () => {
-    const [messages, setMessages] = useState([]);
-    const [inputMessage, setInputMessage] = useState('');
+  const [messages, setMessages] = useState([]);
+  const [inputMessage, setInputMessage] = useState("");
 
-    const sendMessage = () => {
-        if (inputMessage.trim() !== '') {
-            setMessages([...messages, { text: inputMessage, sender: 'user' }]);
-            setInputMessage('');
-        }
-    };
+  const sendMessage = () => {
+    if (inputMessage.trim() !== "") {
+      setMessages([...messages, { text: inputMessage, sender: "user" }]);
+      setInputMessage("");
+    }
+  };
 
-    return (
-        <div className='holder'>
-            <div className='container mt-5'>
-                <div className='card'>
-                    <div className='card-body'>
-                        <div className='chat-container'>
-                            <div className='messages'>
-                                {messages.map((message, index) => (
-                                    <div key={index} className={`message ${message.sender}`}>
-                                        {message.text}
-                                    </div>
-                                ))}
-                            </div>
-                            <div className='input-group mt-3'>
-                                <input type='text' className='form-control' placeholder='Type your message...' value={inputMessage} onChange={(e) => setInputMessage(e.target.value)} />
-                                <button className='btn btn-primary' onClick={sendMessage}>
-                                    Send
-                                </button>
-                            </div>
-                        </div>
+  return (
+    <>
+      <Dropdown className="dropdown-container">
+        <Dropdown.Toggle variant="success" id="dropdown-basic">
+          Available Instructors Today
+        </Dropdown.Toggle>
+
+        <Dropdown.Menu>
+          <Dropdown.Item href="#/action-1">Anakin Skywalker</Dropdown.Item>
+          <Dropdown.Item href="#/action-2">Chico Lachowski</Dropdown.Item>
+          <Dropdown.Item href="#/action-3">Bill Gates</Dropdown.Item>
+        </Dropdown.Menu>
+      </Dropdown>
+
+      <Container>
+        <Row>
+          <Col className="first">
+            <img src={profileIcon} alt="profile" />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="second">
+            <img src={profileIcon} alt="profile" />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="first">
+            <img src={profileIcon} alt="profile" />
+          </Col>
+        </Row>
+        <Row>
+          <Col className="second">
+            <img src={profileIcon} alt="profile" />
+          </Col>
+        </Row>
+      </Container>
+
+      <div className="holder">
+        <div className="container mt-5">
+          <div className="card">
+            <div className="card-body">
+              <div className="chat-container">
+                <div className="messages">
+                  {messages.map((message, index) => (
+                    <div key={index} className={`message ${message.sender}`}>
+                      {message.text}
                     </div>
+                  ))}
                 </div>
+                <div className="input-group mt-3">
+                  <input
+                    type="text"
+                    className="form-control"
+                    placeholder="Type your message..."
+                    value={inputMessage}
+                    onChange={(e) => setInputMessage(e.target.value)}
+                  />
+                  <button className="btn btn-success" onClick={sendMessage}>
+                    Send
+                  </button>
+                </div>
+              </div>
             </div>
+          </div>
         </div>
-    );
+      </div>
+    </>
+  );
 };
 
 export default Inbox;
--- client/src/pages/student/task.jsx
+++ client/src/pages/student/task.jsx
@@ -1,40 +1,87 @@
-import Tab from 'react-bootstrap/Tab';
-import Tabs from 'react-bootstrap/Tabs';
-import Accordion from 'react-bootstrap/Accordion';
-import '../../assets/scss/task.scss';
+import Tab from "react-bootstrap/Tab";
+import Tabs from "react-bootstrap/Tabs";
+import Accordion from "react-bootstrap/Accordion";
+import "../../assets/scss/task.scss";
+import { MDBAccordion, MDBAccordionItem } from "mdb-react-ui-kit";
+import {
+  MDBCard,
+  MDBCardHeader,
+  MDBCardBody,
+  MDBCardTitle,
+  MDBCardText,
+  MDBBtn,
+} from "mdb-react-ui-kit";
 
 export default function Task() {
-    return (
-        <Tabs id='justify-tab' className='mb-3' justify>
-            <Tab className='coach-tab' eventKey='coach' title='Coach'>
-                <div className='coach-content'>
-                    <Accordion>
-                        <Accordion.Item eventKey='0'>
-                            <Accordion.Header>ITE 400</Accordion.Header>
-                            <Accordion.Body>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </Accordion.Body>
-                        </Accordion.Item>
-                        <Accordion.Item eventKey='1'>
-                            <Accordion.Header>ITE 300</Accordion.Header>
-                            <Accordion.Body>
-                                Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat
-                                non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-                            </Accordion.Body>
-                        </Accordion.Item>
-                    </Accordion>
-                </div>
-            </Tab>
-            <Tab className='connect-tab' eventKey='connect' title='Connect'>
-                Tab content for Connect
-            </Tab>
-            <Tab className='check-tab' eventKey='check' title='Check'>
-                Tab content for Check
-            </Tab>
-            <Tab className='missing-tab' eventKey='missing' title='Missing'>
-                Tab content for Missing
-            </Tab>
-        </Tabs>
-    );
+  return (
+    <Tabs id="justify-tab" className="mb-3" justify>
+      <Tab className="coach-tab" eventKey="coach" title="Coach">
+        <div className="coach-content">
+          <MDBAccordion initialActive={1}>
+            <MDBAccordionItem collapseId={1} headerTitle="This Week">
+              <MDBCard className="card-container">
+                <MDBCardHeader>ITE 400</MDBCardHeader>
+                <MDBCardBody>
+                  <MDBCardTitle className="title">
+                    Create a proposal
+                  </MDBCardTitle>
+                  <MDBCardText className="text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+              <MDBCard>
+                <MDBCardHeader>HIS 069</MDBCardHeader>
+                <MDBCardBody>
+                  <MDBCardTitle className="title">Make a poster</MDBCardTitle>
+                  <MDBCardText className="text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={2} headerTitle="Next Week">
+              <MDBCard className="card-container">
+                <MDBCardHeader>ITE 400</MDBCardHeader>
+                <MDBCardBody>
+                  <MDBCardTitle className="title">
+                    Create a proposal
+                  </MDBCardTitle>
+                  <MDBCardText className="text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBAccordionItem>
+            <MDBAccordionItem collapseId={3} headerTitle="Later">
+              <MDBCard className="card-container">
+                <MDBCardHeader>ITE 400</MDBCardHeader>
+                <MDBCardBody>
+                  <MDBCardTitle className="title">
+                    Create a proposal
+                  </MDBCardTitle>
+                  <MDBCardText className="text">
+                    With supporting text below as a natural lead-in to
+                    additional content.
+                  </MDBCardText>
+                </MDBCardBody>
+              </MDBCard>
+            </MDBAccordionItem>
+          </MDBAccordion>
+        </div>
+      </Tab>
+      <Tab className="connect-tab" eventKey="connect" title="Connect">
+        Tab content for Connect
+      </Tab>
+      <Tab className="check-tab" eventKey="check" title="Check">
+        Tab content for Check
+      </Tab>
+      <Tab className="missing-tab" eventKey="missing" title="Missing">
+        Tab content for Missing
+      </Tab>
+    </Tabs>
+  );
 }
