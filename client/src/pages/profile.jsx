05d6c87fa17b547baf4ffa821fed1e75c0310f43
swa-ne <spaulbautista@gmail.com>
2024-01-26 21:15:12 +0800

    Revert changes from Profile.jsx

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,9 +1,17 @@
+import '../assets/scss/profile.scss';
+import profileIcon from '../assets/img/mygirl.jpg';
 
-function ProfileProf() {
+const Profile = () => {
     return (
-        <div className='Profile'>
+        <div className='profile'>
+            <div className='profile-down1'>
+                <img src={profileIcon} alt='profile' />
+                <div className='name'>Zai Alicoben</div>
+                <div className='status'>2nd Year</div>
+                <div className='id'>03-2223-040667</div>
+                <div className='acc'>glba.alicoben.up@phinmaed.com</div>
+            </div>
         </div>
     );
-}
-
-export default ProfileProf;
+};
+export default Profile;
