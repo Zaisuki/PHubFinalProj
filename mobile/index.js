acdca603d059be80e5ec4ef56f4581c20c314cf6
Yurei <glba.alicoben.up@phinmaed.com>
2024-02-04 21:36:34 +0800

    mopb9ileksnjlabsdxjhcvdbjwbgcfxksaygfvb khg

--- mobile/index.js
+++ mobile/index.js
@@ -1,8 +1,6 @@
 import { registerRootComponent } from 'expo';
 import 'react-native-gesture-handler';
 import { AppRegistry } from 'react-native';
-
-import App from './App';
 import { name as appName } from './app.json';
 
 import App from './App';
