31a4c08f8db4ce1353d06ab69d5e5f67b7bc4750
Kirisu3 <146045874+Kirisu3@users.noreply.github.com>
2024-02-06 17:41:27 +0800

    Finished Profile

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -25,22 +25,51 @@ function Profile() {
       </Card>
       <Card className='three'>
     <h1>Block:</h1>
+    <h2>BSIT2-03</h2>
       </Card>
       <Card className='four'>
         <h1>Course:</h1>
         <h2>Semester:</h2>
-        
+        <h3>BSIT-Bachelor of Science in Information<br></br> Technology</h3>
+        <h4>Second</h4>
       </Card>
       <Card className='five'>
         <h1>Personal Data</h1>
         <h2>Last Name</h2>
       </Card>
-      
-      
+      <Card className='six'>
+        <h1>Ventti</h1>
+      </Card>
+      <Card className='seven'>
+        <h1>First Name</h1>
+        <h2>Latte</h2>
+      </Card>
 
-    
+      <Card className='eight'>
+        <h1>Middle Name</h1>
+        <h2>Pls</h2>
+      </Card>
+      
+      <Card className='nine'>
+        <h1>Birth Date</h1>
+        <h2>Aba malay ko sayo</h2>
+      </Card>
+
+      <Card className='ten'>
+        <h1>Contact Number</h1>
+        <h2>Madik ammo</h2>
+      </Card>
      
-        
+      <Card className='eleven'>
+        <h1>Current Address(House#, Street, Brgy, City)</h1>
+        <h2>Keta kyeng bale</h2>
+      </Card>
+
+      <Card className='twelve'>
+        <h1>School Email</h1>
+        <h2>Shanong@gmail.com</h2>
+      </Card>
+
       </Card.Body>
       </div>
       
