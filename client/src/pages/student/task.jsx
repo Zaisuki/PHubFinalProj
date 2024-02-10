8d9b066ede543dbd4b04baf53fe94349eca13789
leyschz <97140645+LeanneDelaCruz@users.noreply.github.com>
2024-02-10 21:15:05 +0800

    added a card inside accordion

--- client/src/pages/student/task.jsx
+++ client/src/pages/student/task.jsx
@@ -2,18 +2,27 @@ import Tab from "react-bootstrap/Tab";
 import Tabs from "react-bootstrap/Tabs";
 import "../../assets/scss/task.scss";
 import Accordion from "react-bootstrap/Accordion";
+import Card from "react-bootstrap/Card";
 
 export default function Task() {
   return (
     <Tabs id="justify-tab" className="mb-3" justify>
-
       <Tab className="coach-tab" eventKey="coach" title="Coach">
         <Accordion defaultActiveKey="0" className="main-holder">
           <Accordion.Item eventKey="0" className="1st">
             <Accordion.Header>This Week</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>This is some text within a card body.</Card.Body>
+              </Card>
+              <Card className="card-within">
+                <Card.Body>
+                  <h4>
+                    CHECK:<span>Title</span>
+                  </h4>
+                  <h5>papa mo blue</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1" className="2nd">
