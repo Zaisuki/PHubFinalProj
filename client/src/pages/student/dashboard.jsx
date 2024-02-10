83929d224aacc09d6c9f0605a1d377157130b3a3
swa-ne <spaulbautista@gmail.com>
2024-02-10 11:43:28 +0800

    Improve design

--- client/src/pages/student/dashboard.jsx
+++ client/src/pages/student/dashboard.jsx
@@ -1,26 +1,28 @@
-import "../../assets/scss/dashboard.scss";
-import Card from "react-bootstrap/Card";
-import SummaryTask from "../../components/summaryTask";
+import '../../assets/scss/dashboard.scss';
+import Card from 'react-bootstrap/Card';
 
 function Dashboard() {
-  return (
-    <Card.Body className="stat-card">
-      <Card className="stat-one">
-        <h1>This Week</h1>
-        <h2 className="num-one">1</h2>
-        <h2 className="num-two">1</h2>
-        <h2 className="num-three">1</h2>
-        <h2 className="num-four">1</h2>
+    return (
+        <Card.Body className='stat-card'>
+            <Card className='stat-one'>
+                <h1>This Week</h1>
+                <h2 className='num-one'>1</h2>
+                <h2 className='num-two'>1</h2>
+                <h2 className='num-three'>1</h2>
+                <h2 className='num-four'>1</h2>
 
-        <h3 className="text-one">Check</h3>
-        <h3 className="text-two">Connect</h3>
-        <h3 className="text-three">Coach</h3>
-        <h3 className="text-four">Missing</h3>
-      </Card>
+                <h3 className='text-one'>Check</h3>
+                <h3 className='text-two'>Connect</h3>
+                <h3 className='text-three'>Coach</h3>
+                <h3 className='text-four'>Missing</h3>
+            </Card>
 
-      <Card className="stat-two"></Card>
-    </Card.Body>
-  );
+            <Card className='stat-two'>
+                <h1>ITE 143</h1>
+                <h2>QUIZZES</h2>
+            </Card>
+        </Card.Body>
+    );
 }
 
 export default Dashboard;
