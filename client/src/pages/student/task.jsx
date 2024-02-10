1d648760bead4de494f66355dac4b17452dd1e55
leyschz <97140645+LeanneDelaCruz@users.noreply.github.com>
2024-02-11 00:04:50 +0800

    Task Tab UI done but needs more polishing later

--- client/src/pages/student/task.jsx
+++ client/src/pages/student/task.jsx
@@ -7,20 +7,27 @@ import Card from "react-bootstrap/Card";
 export default function Task() {
   return (
     <Tabs id="justify-tab" className="mb-3" justify>
+      
+      {/* COACH TAB */}
       <Tab className="coach-tab" eventKey="coach" title="Coach">
         <Accordion defaultActiveKey="0" className="main-holder">
           <Accordion.Item eventKey="0" className="1st">
             <Accordion.Header>This Week</Accordion.Header>
             <Accordion.Body>
               <Card className="card-within">
-                <Card.Body>This is some text within a card body.</Card.Body>
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
               </Card>
               <Card className="card-within">
                 <Card.Body>
-                  <h4>
-                    CHECK:<span>Title</span>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 20</span>
                   </h4>
-                  <h5>papa mo blue</h5>
+                  <h5 className="date-posted">February 11, 2024</h5>
                 </Card.Body>
               </Card>
             </Accordion.Body>
@@ -28,93 +35,162 @@ export default function Task() {
           <Accordion.Item eventKey="1" className="2nd">
             <Accordion.Header>Next Week</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2" className="3rd">
             <Accordion.Header>Later</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
       </Tab>
 
+      {/* CONNECT TAB */}
       <Tab className="connect-tab" eventKey="connect" title="Connect">
         <Accordion defaultActiveKey="0" className="main-holder">
           <Accordion.Item eventKey="0" className="1st">
             <Accordion.Header>This Week</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1" className="2nd">
             <Accordion.Header>Next Week</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2" className="3rd">
             <Accordion.Header>Later</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
       </Tab>
 
+      {/* CHECK TAB */}
       <Tab className="check-tab" eventKey="check" title="Check">
         <Accordion defaultActiveKey="0" className="main-holder">
           <Accordion.Item eventKey="0" className="1st">
             <Accordion.Header>This Week</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1" className="2nd">
             <Accordion.Header>Next Week</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2" className="3rd">
             <Accordion.Header>Later</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
       </Tab>
 
+      {/* MISSING TAB */}
       <Tab className="missing-tab" eventKey="missing" title="Missing">
         <Accordion defaultActiveKey="0" className="main-holder">
           <Accordion.Item eventKey="0" className="1st">
             <Accordion.Header>This Week</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="1" className="2nd">
             <Accordion.Header>Next Week</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
           <Accordion.Item eventKey="2" className="3rd">
             <Accordion.Header>Earlier</Accordion.Header>
             <Accordion.Body>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-              eiusmod tempor
+              <Card className="card-within">
+                <Card.Body>
+                  <h4 className="task-type">
+                    CHECK: <span className="task-title">Module 21</span>
+                  </h4>
+                  <h5 className="date-posted">February 10, 2024</h5>
+                </Card.Body>
+              </Card>
             </Accordion.Body>
           </Accordion.Item>
         </Accordion>
