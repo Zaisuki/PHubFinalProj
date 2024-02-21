66d5aac32b1a79896aacce7eb4a718dc50f5e44d
swa-ne <spaulbautista@gmail.com>
2024-02-22 00:41:43 +0800

    Professors can now post Announcements

--- client/src/pages/professor/feed.jsx
+++ client/src/pages/professor/feed.jsx
@@ -1,117 +1,112 @@
-import React, { useEffect, useRef, useState } from "react";
-import "../../assets/scss/prof-scss/feed.scss";
-import Card from "react-bootstrap/Card";
-import Button from "react-bootstrap/Button";
-import Container from "react-bootstrap/Container";
-import Row from "react-bootstrap/Row";
-import Col from "react-bootstrap/Col";
+import { useEffect, useRef, useState } from 'react';
+import '../../assets/scss/prof-scss/feed.scss';
+import Card from 'react-bootstrap/Card';
+import Button from 'react-bootstrap/Button';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import { postAnnouncement } from '../../services/professor';
 
 function FeedProf() {
-  const titleTextAreaRef = useRef(null);
-  const descriptionTextAreaRef = useRef(null);
-  const [titleVal, setTitleVal] = useState("");
-  const [descriptionVal, setDescriptionVal] = useState("");
-  const [titleCharacterCount, setTitleCharacterCount] = useState(0);
-  const [descriptionCharacterCount, setDescriptionCharacterCount] = useState(0);
-  const titleCharacterLimit = 30;
-  const descriptionCharacterLimit = 200;
+    const titleTextAreaRef = useRef(null);
+    const descriptionTextAreaRef = useRef(null);
+    const [titleVal, setTitleVal] = useState('');
+    const [descriptionVal, setDescriptionVal] = useState('');
+    const [titleCharacterCount, setTitleCharacterCount] = useState(0);
+    const [descriptionCharacterCount, setDescriptionCharacterCount] = useState(0);
+    const titleCharacterLimit = 30;
+    const descriptionCharacterLimit = 500;
 
-  const handleTitleChange = (e) => {
-    const input = e.target.value;
-    if (input.length <= titleCharacterLimit) {
-      setTitleVal(input);
-      setTitleCharacterCount(input.length);
-    }
-  };
+    const handleTitleChange = (e) => {
+        const input = e.target.value;
+        if (input.length <= titleCharacterLimit) {
+            setTitleVal(input);
+            setTitleCharacterCount(input.length);
+        }
+    };
 
-  const handleDescriptionChange = (e) => {
-    const input = e.target.value;
-    if (input.length <= descriptionCharacterLimit) {
-      setDescriptionVal(input);
-      setDescriptionCharacterCount(input.length);
-    }
-  };
+    const handleDescriptionChange = (e) => {
+        const input = e.target.value;
+        if (input.length <= descriptionCharacterLimit) {
+            setDescriptionVal(input);
+            setDescriptionCharacterCount(input.length);
+        }
+    };
+    const handleSubmit = async () => {
+        const result = await postAnnouncement(titleVal, descriptionVal);
+        if (result.message === 'Announcement posted') {
+            // Todo: show gui for success notif
+            setTitleVal('');
+            setDescriptionVal('');
+        }
+    };
+    useEffect(() => {
+        if (titleTextAreaRef.current) {
+            titleTextAreaRef.current.style.height = 'auto';
+            titleTextAreaRef.current.style.height = titleTextAreaRef.current.scrollHeight + 'px';
+        }
+        if (descriptionTextAreaRef.current) {
+            descriptionTextAreaRef.current.style.height = 'auto';
+            descriptionTextAreaRef.current.style.height = descriptionTextAreaRef.current.scrollHeight + 'px';
+        }
+    }, [titleVal, descriptionVal]);
 
-  useEffect(() => {
-    if (titleTextAreaRef.current) {
-      titleTextAreaRef.current.style.height = "auto";
-      titleTextAreaRef.current.style.height =
-        titleTextAreaRef.current.scrollHeight + "px";
-    }
-    if (descriptionTextAreaRef.current) {
-      descriptionTextAreaRef.current.style.height = "auto";
-      descriptionTextAreaRef.current.style.height =
-        descriptionTextAreaRef.current.scrollHeight + "px";
-    }
-  }, [titleVal, descriptionVal]);
-
-  return (
-    <Container className="feed-post">
-      <Row>
-        <Col sm={7}>
-          <Card className="feed-one">
-            <h1></h1>
-          </Card>
-
-          <Card className="feed-two">
-            <h1></h1>
-          </Card>
-
-          <Card className="feed-three">
-            <h1></h1>
-          </Card>
-        </Col>
-
-        <Col>
-          <Card className="feed-post-one">
+    return (
+        <Container className='feed-post'>
             <Row>
-              <Col>
-                <div className="text-one">
-                  <h1>CREATE A POST</h1>
-                </div>
+                <Col sm={7}>
+                    <Card className='feed-one'>
+                        <h1></h1>
+                    </Card>
 
-                <div className="h2-text">
-                  <h2>Title</h2>
-                </div>
-                <div className="input-placeholder">
-                  <textarea
-                    className="p-2 bg-neutral-700 active active:outline-none focus:outline-none w-100 h-80"
-                    placeholder="Type something.."
-                    value={titleVal}
-                    onChange={handleTitleChange}
-                    rows="1"
-                    ref={titleTextAreaRef}
-                  ></textarea>
-                  <p className="word-count">{titleCharacterCount}/{titleCharacterLimit}</p>
-                </div>
-                <div className="h2-text-one">
-                  <h2>Description</h2>
+                    <Card className='feed-two'>
+                        <h1></h1>
+                    </Card>
 
-                  <div className="input-placeholder-one">
-                    <div className="textArea">
-                      <textarea
-                        className="p-2 bg-neutral-700 active active:outline-none focus:outline-none w-100 h-80"
-                        placeholder="Type something.."
-                        value={descriptionVal}
-                        onChange={handleDescriptionChange}
-                        rows="1"
-                        ref={descriptionTextAreaRef}
-                      ></textarea>
-                      <p className="word-count">{descriptionCharacterCount}/{descriptionCharacterLimit}</p>
-                      <Button
-                        className="create-post"
-                        variant="success"
-                      ></Button>
-                    </div>
-                  </div>
-                </div>
-              </Col>
+                    <Card className='feed-three'>
+                        <h1></h1>
+                    </Card>
+                </Col>
+
+                <Col>
+                    <Card className='feed-post-one'>
+                        <Row>
+                            <Col>
+                                <div className='text-one'>
+                                    <h1>CREATE A POST</h1>
+                                </div>
+
+                                <div className='h2-text'>
+                                    <h2>Title</h2>
+                                </div>
+                                <div className='input-placeholder'>
+                                    <input className='p-2 bg-neutral-700 active active:outline-none focus:outline-none w-100 h-80' placeholder='Type something..' value={titleVal} onChange={handleTitleChange} rows='1' ref={titleTextAreaRef} />
+                                    <p className='word-count'>
+                                        {titleCharacterCount}/{titleCharacterLimit}
+                                    </p>
+                                </div>
+                                <div className='h2-text-one'>
+                                    <h2>Description</h2>
+
+                                    <div className='input-placeholder-one'>
+                                        <div className='textArea'>
+                                            <textarea className='p-2 bg-neutral-700 active active:outline-none focus:outline-none w-100 h-80' placeholder='Type something..' value={descriptionVal} onChange={handleDescriptionChange} rows='1' ref={descriptionTextAreaRef}></textarea>
+                                            <p className='word-count'>
+                                                {descriptionCharacterCount}/{descriptionCharacterLimit}
+                                            </p>
+                                            <Button className='create-post' variant='success' onClick={handleSubmit}>
+                                                Post
+                                            </Button>
+                                        </div>
+                                    </div>
+                                </div>
+                            </Col>
+                        </Row>
+                    </Card>
+                </Col>
             </Row>
-          </Card>
-        </Col>
-      </Row>
-    </Container>
-  );
+        </Container>
+    );
 }
 
 export default FeedProf;
---
+++ client/src/services/professor.js
@@ -0,0 +1,28 @@
+import axios from 'axios';
+import { cookies } from './entry';
+
+const apiURL = import.meta.env.VITE_API_BASE_URL;
+
+export const postAnnouncement = async (titleVal, descriptionVal) => {
+    const response = await axios
+        .post(
+            `${apiURL}/professor/announcement`,
+            {
+                header: titleVal,
+                announcement: descriptionVal,
+                classID: 'public',
+            },
+            {
+                withCredentials: true,
+                headers: {
+                    authorization: cookies.get('authorization'),
+                },
+            }
+        )
+        .then((response) => response.data)
+        .catch((error) => {
+            throw error;
+        });
+    console.log(response);
+    return response;
+};
