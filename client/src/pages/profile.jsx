c885952c171ee7a6f9e5c21becc8ffe9abec3c98
swa-ne <spaulbautista@gmail.com>
2024-02-21 23:59:38 +0800

    Users can now view their profile page and see their information

--- client/src/pages/profile.jsx
+++ client/src/pages/profile.jsx
@@ -1,9 +1,9 @@
-import "../assets/scss/profile.scss";
-import picon from "../assets/img/mygirl.jpg";
-import Card from "react-bootstrap/Card";
-import { profile } from "../services/user";
-import Loading from "./loading";
-import { useEffect, useState } from "react";
+import '../assets/scss/profile.scss';
+import picon from '../assets/img/mygirl.jpg';
+import Card from 'react-bootstrap/Card';
+import { profile } from '../services/user';
+import Loading from './loading';
+import { useEffect, useState } from 'react';
 
 function Profile() {
     const [data, setData] = useState(null);
@@ -11,85 +11,84 @@ function Profile() {
 
     useEffect(() => {
         const fetchData = async () => {
-          try {
-            const response = await profile();
-            setData(response);
-            console.log(data)
-            setUserInformation(response.userInformation[0])
-          } catch (error) {
-            console.error('Error fetching data:', error);
-          }
+            try {
+                const response = await profile();
+                setData(response);
+                setUserInformation(() => (response.userType === 'student' ? response.studentInformation : response.userType === 'professor' ? response.professorInformation : response.adminInformation));
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
         };
-    
+
         fetchData();
-      }, []);
+    }, []);
 
-      if (!data) {
+    if (!data) {
         return <Loading />;
-      }
-    
-  return (
-    <Card.Body className="card-booty">
-      <Card className="one">
-        <img className="profile-img" src={picon} alt="profile" />
-        <h1>{userInformation.firstName} {userInformation.middleName} {userInformation.lastName}</h1>
-        <h2>{userInformation.personalNumber}</h2>
-        <h3>{data.personalEmail}</h3>
-        <div className="line"></div>
-      </Card>
-      <Card className="two">
-        <h1>Year/Grade:</h1>
-        <h2>2nd</h2>
-      </Card>
-      <Card className="three">
-        <h1>Block:</h1>
-        <h2>{userInformation.section}</h2>
-      </Card>
-      <Card className="four">
-        <h1>Course:</h1>
-        <h2>Semester:</h2>
-        <h3>
-          {userInformation.course}
-        </h3>
-        <h4>Second</h4>
-      </Card>
-      <Card className="five">
-        <h1>Personal Data</h1>
-        <h2>Last Name</h2>
-      </Card>
-      <Card className="six">
-        <h1>{userInformation.lastName}</h1>
-      </Card>
-      <Card className="seven">
-        <h1>First Name</h1>
-        <h2>{userInformation.firstName}</h2>
-      </Card>
+    }
 
-      <Card className="eight">
-        <h1>Middle Name</h1>
-        <h2>{userInformation.middleName}</h2>
-      </Card>
+    return (
+        <Card.Body className='card-booty'>
+            <Card className='one'>
+                <img className='profile-img' src={picon} alt='profile' />
+                <h1>
+                    {userInformation.firstName} {userInformation.middleName} {userInformation.lastName}
+                </h1>
+                <h2>{userInformation.personalNumber}</h2>
+                <h3>{data.personalEmail}</h3>
+                <div className='line'></div>
+            </Card>
+            <Card className='two'>
+                <h1>Year/Grade:</h1>
+                <h2>2nd</h2>
+            </Card>
+            <Card className='three'>
+                <h1>Block:</h1>
+                <h2>{userInformation.section}</h2>
+            </Card>
+            <Card className='four'>
+                <h1>Course:</h1>
+                <h2>Semester:</h2>
+                <h3>{userInformation.course}</h3>
+                <h4>Second</h4>
+            </Card>
+            <Card className='five'>
+                <h1>Personal Data</h1>
+                <h2>Last Name</h2>
+            </Card>
+            <Card className='six'>
+                <h1>{userInformation.lastName}</h1>
+            </Card>
+            <Card className='seven'>
+                <h1>First Name</h1>
+                <h2>{userInformation.firstName}</h2>
+            </Card>
 
-      <Card className="nine">
-        <h1>Birth Date</h1>
-        <h2>{userInformation.birthday}</h2>
-      </Card>
+            <Card className='eight'>
+                <h1>Middle Name</h1>
+                <h2>{userInformation.middleName}</h2>
+            </Card>
 
-      <Card className="ten">
-        <h1>Contact Number</h1>
-        <h2>{userInformation.personalNumber}</h2>
-      </Card>
+            <Card className='nine'>
+                <h1>Birth Date</h1>
+                <h2>{userInformation.birthday}</h2>
+            </Card>
 
-      <Card className="eleven">
-        <h1>Current Address(House#, Street, Brgy, City)</h1>
-        <h2>{userInformation.address}</h2>
-      </Card>
+            <Card className='ten'>
+                <h1>Contact Number</h1>
+                <h2>{userInformation.personalNumber}</h2>
+            </Card>
 
-      <Card className="twelve">
-        <h1>School Email</h1>
-        <h2>{data.schoolEmail}</h2>
-      </Card>
-    </Card.Body>
-  );
+            <Card className='eleven'>
+                <h1>Current Address(House#, Street, Brgy, City)</h1>
+                <h2>{userInformation.address}</h2>
+            </Card>
+
+            <Card className='twelve'>
+                <h1>School Email</h1>
+                <h2>{data.schoolEmail}</h2>
+            </Card>
+        </Card.Body>
+    );
 }
 export default Profile;
--- client/src/services/user.js
+++ client/src/services/user.js
@@ -1,23 +1,20 @@
-import axios from "axios";
+import axios from 'axios';
 import { cookies } from './entry';
 
 const apiURL = import.meta.env.VITE_API_BASE_URL;
 
 export const profile = async () => {
-  const response = await axios
-    .get(
-      `${apiURL}/user/profile`,
-      {
-        withCredentials: true,
-        headers: {
-          authorization: cookies.get("authorization"),
-        },
-      }
-    )
-    .then((response) => response.data)
-    .catch((error) => {
-      throw error;
-    });
-  console.log(response);
-  return response;
+    const response = await axios
+        .get(`${apiURL}/user/profile`, {
+            withCredentials: true,
+            headers: {
+                authorization: cookies.get('authorization'),
+            },
+        })
+        .then((response) => response.data)
+        .catch((error) => {
+            throw error;
+        });
+    console.log(response);
+    return response;
 };
