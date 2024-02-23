bf4df980c0a68f0f6521f514680fe5931ea9ce93
swa-ne <spaulbautista@gmail.com>
2024-02-23 23:06:00 +0800

    Professor can now upload images/files

--- api/server/services/upload.ts
+++ api/server/services/upload.ts
@@ -1,14 +1,26 @@
-import multer from 'multer'
-import path from 'path'
+import multer from 'multer';
+import path from 'path';
 import express, { Express, Request, Response } from 'express';
-
 
 const storage = multer.diskStorage({
     destination: (req: Request, file: Express.Multer.File, cb) => {
-        cb(null, './Images');
+        cb(null, './upload');
     },
     filename: (req, file, cb) => {
-        cb(null, Date.now() + path.extname(file.originalname))
+        cb(null, Date.now() + path.extname(file.originalname));
+    },
+});
+
+const fileFilter = (req: Request, file: Express.Multer.File, cb: any) => {
+    const allowedFileTypes = ['image/jpeg', 'image/png', 'image/jpg', 'application/msword', 'application/vnd.openxmlformats-officedocument.wordprocessingml.document', 'application/pdf'];
+
+    if (allowedFileTypes.includes(file.mimetype)) {
+        cb(null, true);
+    } else {
+        cb(new Error('Invalid file type. Only images (jpeg, jpg, png), documents (doc, docx, pdf) are allowed.'), false);
     }
-})
-export const upload = multer({storage: storage})
+};
+export const upload = multer({
+    storage: storage,
+    fileFilter: fileFilter,
+});
