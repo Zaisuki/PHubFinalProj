1e3080ef8e26dc9cccad6f6bdd619cc4bad45567
swa-ne <spaulbautista@gmail.com>
2024-02-29 02:52:54 +0800

    Created functions where it determines who has already turned in, been graded, and those who haven't turned in yet.

--- api/server/models/classModel/studentClass.ts
+++ api/server/models/classModel/studentClass.ts
@@ -1,124 +1,112 @@
-import mongoose, { Schema, InferSchemaType } from "mongoose";
+import mongoose, { Schema, InferSchemaType } from 'mongoose';
 
 const studentCheckSubmissionSchema = new Schema(
-  {
-    student: {
-      type: Schema.Types.ObjectId,
-      ref: "Student",
-      default: null,
+    {
+        student: {
+            type: Schema.Types.ObjectId,
+            ref: 'Student',
+            default: null,
+        },
+        attachment: [
+            {
+                type: Buffer,
+            },
+        ],
+        score: {
+            type: Number,
+            default: null,
+        },
+        task: {
+            type: Schema.Types.ObjectId,
+            ref: 'Check',
+            default: null,
+        },
     },
-    attachment: [
-      {
-        type: Buffer,
-      },
-    ],
-    score: {
-      type: Number,
-      default: 0,
-    },
-    class: {
-      type: Schema.Types.ObjectId,
-      ref: "Class",
-      default: null,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 const studentConnectSubmissionSchema = new Schema(
-  {
-    student: {
-      type: Schema.Types.ObjectId,
-      ref: "Student",
-      default: null,
+    {
+        student: {
+            type: Schema.Types.ObjectId,
+            ref: 'Student',
+            default: null,
+        },
+        answer: {
+            type: Schema.Types.ObjectId,
+            ref: 'ConnectChoices',
+            default: null,
+        },
+        score: {
+            type: Number,
+            default: null,
+        },
+        class: {
+            type: Schema.Types.ObjectId,
+            ref: 'Class',
+            default: null,
+        },
     },
-    answer: {
-      type: Schema.Types.ObjectId,
-      ref: "ConnectChoices",
-      default: null,
-    },
-    score: {
-      type: Number,
-      default: 0,
-    },
-    class: {
-      type: Schema.Types.ObjectId,
-      ref: "Class",
-      default: null,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
 const studentCoachViewSchema = new Schema(
-  {
-    student: {
-      type: Schema.Types.ObjectId,
-      ref: "Student",
-      default: null,
+    {
+        student: {
+            type: Schema.Types.ObjectId,
+            ref: 'Student',
+            default: null,
+        },
+        class: {
+            type: Schema.Types.ObjectId,
+            ref: 'Class',
+            default: null,
+        },
     },
-    class: {
-      type: Schema.Types.ObjectId,
-      ref: "Class",
-      default: null,
-    },
-  },
-  {
-    timestamps: true,
-  }
+    {
+        timestamps: true,
+    }
 );
-export const StudentConnectSubmission = mongoose.model(
-  "StudentConnectSubmission",
-  studentConnectSubmissionSchema
-);
-export const StudentCheckSubmission = mongoose.model(
-  "StudentCheckSubmission",
-  studentCheckSubmissionSchema
-);
-export const StudentCoachView = mongoose.model(
-  "StudentCoachView",
-  studentCoachViewSchema
-);
+export const StudentConnectSubmission = mongoose.model('StudentConnectSubmission', studentConnectSubmissionSchema);
+export const StudentCheckSubmission = mongoose.model('StudentCheckSubmission', studentCheckSubmissionSchema);
+export const StudentCoachView = mongoose.model('StudentCoachView', studentCoachViewSchema);
 
 const studentSubjectsSchema = new Schema({
-  student: {
-    type: Schema.Types.ObjectId,
-    ref: "Student",
-    default: null,
-  },
-  class: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "Class",
-      default: null,
+    student: {
+        type: Schema.Types.ObjectId,
+        ref: 'Student',
+        default: null,
     },
-  ],
-  studentCheckSubmission: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "StudentCheckSubmission",
-      default: null,
-    },
-  ],
-  studentConnectSubmission: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "StudentConnectSubmission",
-      default: null,
-    },
-  ],
-  studentCoachView: [
-    {
-      type: Schema.Types.ObjectId,
-      ref: "StudentCoachView",
-      default: null,
-    },
-  ],
+    class: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'Class',
+            default: null,
+        },
+    ],
+    studentCheckSubmission: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'StudentCheckSubmission',
+            default: null,
+        },
+    ],
+    studentConnectSubmission: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'StudentConnectSubmission',
+            default: null,
+        },
+    ],
+    studentCoachView: [
+        {
+            type: Schema.Types.ObjectId,
+            ref: 'StudentCoachView',
+            default: null,
+        },
+    ],
 });
 
-export const StudentSubjects = mongoose.model(
-  "StudentSubjects",
-  studentSubjectsSchema
-);
+export const StudentSubjects = mongoose.model('StudentSubjects', studentSubjectsSchema);
