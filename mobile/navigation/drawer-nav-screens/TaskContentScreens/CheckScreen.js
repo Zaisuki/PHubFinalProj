7d68971e672a83fdecf119e2bd6e65f218895d77
Renek <jaes.ang.up@phinmaed.com>
2024-03-01 00:25:25 +0800

    NewTaskScreen working. Log out button is in profile.

--- mobile/navigation/drawer-nav-screens/TaskContentScreens/CheckScreen.js
+++ mobile/navigation/drawer-nav-screens/TaskContentScreens/CheckScreen.js
@@ -1,8 +1,87 @@
 import * as React from 'react';
-import { Text } from 'react-native';
+import { Text, View, Image, Dimensions } from 'react-native';
 import { List, Card, Button } from 'react-native-paper';
+import {NavigationContainer} from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { pakyu } from '../../../mgadimahanapnaimage';
+import * as DocumentPicker from 'expo-document-picker';
 
-const CheckScreen =  ({navigation}) => {
+const Stack = createNativeStackNavigator();
+
+const dimensions = Dimensions.get('window');   
+const imageWidth = dimensions.width;
+const imageHeight = dimensions.height;
+
+
+
+const NewTask = ({navigation, route}) => {
+  const selectDoc = async () => {
+        try {
+          const doc = await DocumentPicker.getDocumentAsync({
+            type: "application/pdf",
+            multiple: true
+             
+          });
+
+          const formData = new FormData();
+          const assets = doc.assets
+          if(!assets) return
+
+          const file = assets[0];
+
+          const pdfFile = {
+            name: file.name.split(".")[0],
+            uri: file.uri,
+            type: file.mimeType,
+            size:file.size
+          };
+
+          formData.append("pdfFile", pdfFile);
+        } catch (err) {
+          
+          console.log("User Cancelled the upload", err);
+        }
+      }
+    return (
+      <View>
+      <Card style ={{
+        borderWidth: 3,
+        marginTop: 5,
+        marginBottom: 5
+      }}>
+        <Card.Title title = "Module 24" subtitle = "Feb 24, 2024"/>
+        <Card.Content>
+          <Text variant="bodyMedium"> Please blahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblahblah {"\n"}</Text>
+        </Card.Content>
+        <Card.Cover source = {{uri: 'https://picsum.photos/700'}}/>
+        <Text variant="bodyMedium"> {"\n"} Tas eto isunod niyo rin para mas mahirapan kayo ganitoganyanganyanganyanganyanganyanganyangan{"\n"}yanganyanganyanganyanganyanganyan</Text>
+      </Card>
+
+      <Card style ={{
+        borderWidth: 3,
+        marginTop: 5,
+        marginBottom: 5
+      }}>
+        <Card.Content>
+          <Text>Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag Balagbag </Text>
+        </Card.Content>
+      </Card>
+      <Button onPress={selectDoc} mode = 'contained'> 
+      Add Work +
+      </Button>
+      
+      <Button mode='outlined' style = {{
+        margin: 5
+      }}>
+        Mark as done
+      </Button>
+      </View>
+  
+  );
+    
+};
+
+const CheckScreenContent =  ({navigation, route}) => {
     const [expanded, setExpanded] = React.useState(true);
   
     const handlePress = () => setExpanded(!expanded);
@@ -14,7 +93,7 @@ const CheckScreen =  ({navigation}) => {
           expanded={expanded}
           onPress={handlePress}>
           
-          <Card onPress = {() => console.log('Pressed')} 
+          <Card onPress = {() => navigation.push("NewTask")} 
           style={{
             borderRadius: 15, 
             borderWidth: 2, 
@@ -76,7 +155,7 @@ const CheckScreen =  ({navigation}) => {
             height: 100, 
             justifyContent: 'center',
             marginTop: 5,
-            marginBottom: 5,
+            marginTop: 5,
             borderColor: '#d3d3d3',
           }} 
           contentStyle= {{
@@ -147,10 +226,22 @@ const CheckScreen =  ({navigation}) => {
             fontSize: 10,
             textAlign: 'center'
           }} />
-      </Card>
+        </Card>
         </List.Accordion>
       </List.Section>
     );
   };
   
-  export default CheckScreen;
+  export default function CheckScreen() {
+    return (
+      <NavigationContainer independent ={true}>
+        <Stack.Navigator screenOptions={{
+          headerShown: false,
+        }}>
+          <Stack.Screen name = "CheckScreenContent" component={CheckScreenContent}/>
+          <Stack.Screen name = "NewTask" component = {NewTask}/>
+
+        </Stack.Navigator>
+      </NavigationContainer>
+    )
+  }
