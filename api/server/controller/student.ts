92c16ee824037492b1ecf0d140c470c9688a3a89
swa-ne <spaulbautista@gmail.com>
2024-02-29 16:46:51 +0800

    Created API functions where students can submit their tasks.

--- api/server/controller/student.ts
+++ api/server/controller/student.ts
@@ -1,6 +1,6 @@
 import express, { Express, Request, Response } from 'express';
 import { User } from '../middleware/authentication';
-import { getAnnouncement, getCheck, getCoach, getConnect } from '../services/student';
+import { getAnnouncement, getCheck, getCoach, getConnect, submitCheck, submitConnect } from '../services/student';
 
 export const getAnnouncementController = async (req: Request & { user?: User }, res: Response) => {
     try {
@@ -35,6 +35,29 @@ export const getCoachController = async (req: Request & { user?: User }, res: Re
         const studentID = req.user?.userID;
         const result = await getCoach(studentID);
         return res.status(200).json({ 'message': result });
+    } catch (error) {
+        return res.status(500).json({ 'message': 'Internal Server Error' });
+    }
+};
+
+export const submitCheckController = async (req: Request & { user?: User }, res: Response) => {
+    try {
+        const userID = req.user?.userID;
+        const { taskID } = req.body;
+        //@ts-ignore
+        const result = await submitCheck(taskID, userID, req.files);
+        return res.status(result.httpCode).json({ 'message': result.message });
+    } catch (error) {
+        return res.status(500).json({ 'message': 'Internal Server Error' });
+    }
+};
+
+export const submitConnectController = async (req: Request & { user?: User }, res: Response) => {
+    try {
+        const userID = req.user?.userID;
+        const { taskID, answerID } = req.body;
+        const result = await submitConnect(taskID, userID, answerID);
+        return res.status(result.httpCode).json({ 'message': result.message });
     } catch (error) {
         return res.status(500).json({ 'message': 'Internal Server Error' });
     }
--- api/server/routes/student.ts
+++ api/server/routes/student.ts
@@ -2,15 +2,17 @@ import express, { Express, Request, Response, Router } from 'express';
 
 import { authenticateToken } from '../middleware/authentication';
 import { studentOnly } from '../middleware/student';
-import { getAnnouncementController, getCheckController, getCoachController, getConnectController } from '../controller/student';
+import { getAnnouncementController, getCheckController, getCoachController, getConnectController, submitCheckController, submitConnectController } from '../controller/student';
 import { getCheckTaskController, getCoachTaskController, getConnectTaskController } from '../controller/professor';
 
 const router = Router();
 
 router.get('/feed', authenticateToken, studentOnly, getAnnouncementController);
 router.get('/check', authenticateToken, studentOnly, getCheckController);
+router.post('/check', authenticateToken, studentOnly, submitCheckController);
 router.get('/check/task', authenticateToken, studentOnly, getCheckTaskController);
 router.get('/connect', authenticateToken, studentOnly, getConnectController);
+router.post('/connect', authenticateToken, studentOnly, submitConnectController);
 router.get('/connect/task', authenticateToken, studentOnly, getConnectTaskController);
 router.get('/coach', authenticateToken, studentOnly, getCoachController);
 router.get('/coach/task', authenticateToken, studentOnly, getCoachTaskController);
