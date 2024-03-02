90c18d24ce88fd3ef2b064d993d72e6828d9833e
Renek <jaes.ang.up@phinmaed.com>
2024-03-02 13:38:29 +0800

    Students can now view their courses/subjects in course tab.

--- mobile/navigation/drawer-nav-screens/CourseScreen.js
+++ mobile/navigation/drawer-nav-screens/CourseScreen.js
@@ -1,25 +1,47 @@
-import React from 'react';
-import {View, Text, Button, StyleSheet} from 'react-native';
+import React, { useEffect, useState } from 'react';
+import { course } from '../../services/user';
+import { StyleSheet, Text, View } from 'react-native';
 
 const CourseScreen = ({navigation}) => {
+    const [subjects, setSubjects] = useState([]);
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await course();
+                setSubjects(() => (response.userType === 'student' ? response.userDetails.studentSubjects.class : response.userDetails.professorHandledClass.class));
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
+
+        fetchData();
+    }, []);
     return (
-        <View style={styles.container}>
-            <Text>Course Screen</Text>
-            <Button
-            title='Click Here'
-            onPress={() => ('Button Clicked!')}
-            />
+        <View>
+        <View>
+            {subjects ? (
+                subjects.map((subject) => (
+                    <View key={subject._id}>
+                        <Text>
+                            {subject.subject.subjectCode}: <Text>{subject.block}</Text>
+                        </Text>
+                        <View>
+                            <Text> Instructor </Text>
+                            <Text>
+                                {subject.professor.firstName} {subject.professor.lastName}
+                            </Text>
+                            {/* <Button onPress={() => navigate(`/course-new/${subject._id}`)} type='button' className='btn btn-dark' data-mdb-ripple-init>
+                                View */}
+                            {/* </Button> */}
+                        </View>
+                    </View>
+                ))
+            ) : (
+                <Text>No Announcement</Text>
+            )}
         </View>
+    </View>
     )
 };
 
 export default CourseScreen;
-
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#8fcbbc'
-    },
-});
