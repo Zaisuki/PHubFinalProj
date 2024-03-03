88282fff22314b4a4529cd2a627ea300831e0f4d
swa-ne <spaulbautista@gmail.com>
2024-03-03 17:24:27 +0800

    Revert changes from feed.jsx and add a variable that contains the user's full name.

--- client/src/pages/student/feed.jsx
+++ client/src/pages/student/feed.jsx
@@ -1,85 +1,59 @@
-import Card from "react-bootstrap/Card";
-import "../../assets/scss/feed.scss";
-import { useEffect, useState } from "react";
-import { convertDate } from "../../utils/convertDate";
-import { feed, profile } from "../../services/user";
+import Card from 'react-bootstrap/Card';
+import '../../assets/scss/feed.scss';
+import feedImg from '../../assets/img/feed.png';
+import { useEffect, useState } from 'react';
+import { convertDate } from '../../utils/convertDate';
+import { feed } from '../../services/user';
+import { cookies } from '../../services/entry';
 
 function Feed() {
-  const [announcements, setAnnouncements] = useState([]);
-  const [userInformation, setUserInformation] = useState(null);
+    const [announcements, setAnnouncements] = useState([]);
+    const userFullName = cookies.get('userFullName');
+    useEffect(() => {
+        const fetchData = async () => {
+            try {
+                const response = await feed();
+                setAnnouncements(response.message);
+            } catch (error) {
+                console.error('Error fetching data:', error);
+            }
+        };
 
-  useEffect(() => {
-    const fetchData = async () => {
-      try {
+        fetchData();
+    }, []);
 
-        const response = await feed();
-        setAnnouncements(response.message);
+    return (
+        <Card className='feed-student'>
+            <Card className='feed-student-body'>
+                <img className='feed-img' src={feedImg} alt='feed' />
+                <h1></h1>
+                {announcements ? (
+                    announcements.map((announcement) => (
+                        <Card className='feed-content' key={announcement._id}>
+                            <div className='header-announcement'>
+                                <h1>{announcement.header}</h1>
+                                <div className='header-time'>
+                                    <h6>{convertDate(announcement.createdAt)[0]}</h6>
+                                    <h6>{convertDate(announcement.createdAt)[1]}</h6>
+                                </div>
+                            </div>
 
-  
-        const userProfile = await profile();
-        setUserInformation(() => {
-          if (userProfile.userType === "student") {
-            return userProfile.studentInformation;
-          } else if (userProfile.userType === "professor") {
-            return userProfile.professorInformation;
-          } else {
-            return userProfile.adminInformation;
-          }
-        });
-      } catch (error) {
-        console.error("Error fetching data:", error);
-      }
-    };
+                            <div className='announce'>
+                                <p>{announcement.announcement}</p>
+                                {announcement.class && <h1>{announcement.class.subject.subjectCode}</h1>}
+                            </div>
 
-    fetchData();
-  }, []);
-
-  return (
-    <div className="feed-container">
-      <div className="left-column">
-        <Card className="feed-student">
-          <div className="feed-header">
-            <h1 className="user">
-              Welcome to Phinma Hub, <span>{userInformation?.firstName}</span>.
-            </h1>
-          </div>
-          <div className="feed-student-body">
-            <div className="feed-lang">
-              {announcements ? (
-                announcements.map((announcement) => (
-                  <Card className="feed-content" key={announcement._id}>
-                    <div className="header-announcement">
-                      <h1>{announcement.header}</h1>
-                      <div className="header-time">
-                        {convertDate(announcement.createdAt)}
-                      </div>
-                    </div>
-                    <div className="announce">
-                      <p>{announcement.announcement}</p>
-                      {announcement.class && (
-                        <h1>{announcement.class.subject.subjectCode}</h1>
-                      )}
-                    </div>
-                    <h1 className="professor">
-                      {announcement.professor.firstName}{" "}
-                      {announcement.professor.lastName}
-                    </h1>
-                  </Card>
-                ))
-              ) : (
-                <p className="feed-no-announcement">No announcements</p>
-              )}
-            </div>
-          </div>
-      <div className="right-column">
-        <h1>ak</h1>
-
-      </div>
+                            <h1 className='professor'>
+                                {announcement.professor.firstName} {announcement.professor.lastName}
+                            </h1>
+                        </Card>
+                    ))
+                ) : (
+                    <p className='feed-no-announcement'></p>
+                )}
+            </Card>
         </Card>
-      </div>
-    </div>
-    
-  );
+    );
 }
 
 export default Feed;
