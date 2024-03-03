8b9868e86fe8ca854b2ca1369c72eb1d72e5824b
Kirisu3 <146045874+Kirisu3@users.noreply.github.com>
2024-03-04 00:24:59 +0800

    Re-created the Create Class

--- client/src/pages/admin/createClass.jsx
+++ client/src/pages/admin/createClass.jsx
@@ -14,19 +14,83 @@ function CreateClass() {
             <Form.Control placeholder="Enter" />
           </Col>
         </Row>
-        <Row>
-          <Col sm={5}>
-            <Form.Label>Block</Form.Label>
-            <Form.Control placeholder="Enter" />
-          </Col>
-        </Row>
-        <Row>
-          <Col sm={4}>
-            <Form.Label>Subject ID</Form.Label>
-            <Form.Control placeholder="Enter" />
-          </Col>
-        </Row>
       </Container>
+
+      <div className="assigned-class">
+        <p>Assigned Class</p>
+        </div>
+
+      <Form className="blocks">
+      {['checkbox'].map((type) => (
+        <div className="for-class" key={`default-${type}`} >
+          <Form.Check
+            type={type}
+            id={`default-${type}`}
+            label={`Block 1`}
+          />
+
+          <Form.Check
+            type={type}
+            id={`disabled-default-${type}`}
+            label={`Block 2`}
+          />
+          <Form.Check
+            type={type}
+            id={`disabled-default-${type}`}
+            label={`Block 3`}
+          />
+          <Form.Check
+            type={type}
+            id={`disabled-default-${type}`}
+            label={`Block 4`}
+          />
+          <Form.Check
+            type={type}
+            id={`disabled-default-${type}`}
+            label={`Block 5`}
+          />
+        </div>
+        
+      ))}
+      </Form>
+      
+      <div className="assigned-subs">
+        <p>Assigned Subjects</p>
+        </div>
+
+      <Form className="subs">
+      {['checkbox'].map((type) => (
+        <div className="for-class" key={`default-${type}`} >
+          <Form.Check
+            type={type}
+            id={`default-${type}`}
+            label={`ITE 400`}
+          />
+
+          <Form.Check
+            type={type}
+            id={`disabled-default-${type}`}
+            label={`ITE 393`}
+          />
+          <Form.Check
+            type={type}
+            id={`disabled-default-${type}`}
+            label={`ITE 308`}
+          />
+          <Form.Check
+            type={type}
+            id={`disabled-default-${type}`}
+            label={`ITE 300`}
+          />
+          <Form.Check
+            type={type}
+            id={`disabled-default-${type}`}
+            label={`ITE292`}
+          />
+        </div>
+        
+      ))}
+      </Form>
     </div>
   );
 }
